e7455779b7fcb40cfe6949730abf436290586687
Wolfleader <james.morgan@iinet.net.au>
2022-08-11 15:59:24 +0800

    got a pipe

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let img;
+
+class Entity {}
+
+function preload() {
+  img = loadImage("assets/pipe.png");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(900, 450);
 }
 
 function draw() {
   background(220);
+
+  image(img, 0, 400, 69, 100);
+  ellipse(50, 50, 80, 65).fill("#fae502").stroke(0, 0);
 }
